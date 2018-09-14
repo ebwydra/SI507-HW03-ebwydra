7d2dfd91534ce787a3ce7c3848eba52ecedaa9ef
ebwydra <ebwydra@gmail.com>
2018-09-14 11:14:51 -0400

    Checks if input is a question and enables quit:

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,9 @@
 def ask_question():
-    question = input("What is your question? ")
+    question = ""
+    while question != "quit":
+        question = input("What is your question? ")
+        if question == "quit":
+            print("Goodbye.")
+        elif question[-1] != "?":
+            print("I'm sorry, I can only answer questions.")
     return question
