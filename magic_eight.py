d93e034762c08450b0966964ba4b2df43e5b92eb
ebwydra <ebwydra@gmail.com>
2018-09-14 11:41:04 -0400

    fix

--- magic_eight.py
+++ magic_eight.py
@@ -8,6 +8,6 @@ def ask_question():
             print("Goodbye.")
         elif question[-1] != "?":
             print("I'm sorry, I can only answer questions.")
-
-        answers = ["It is certain.","It is decidedly so.","Without a doubt.","Yes-definitely.","You may rely on it.","As I see it, yes.", "Most likely.","Outlook good.","Yes.","Ask again later.","Reply hazy, try again","Concentrate and ask again","Don't count on it.","Very doubtful","My sources say no"]
-        return random.choice(answers)
+        else:
+            answers = ["It is certain.","It is decidedly so.","Without a doubt.","Yes-definitely.","You may rely on it.","As I see it, yes.", "Most likely.","Outlook good.","Yes.","Ask again later.","Reply hazy, try again","Concentrate and ask again","Don't count on it.","Very doubtful","My sources say no"]
+            return random.choice(answers)
