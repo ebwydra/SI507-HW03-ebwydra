be6aa4012b82ffed6ed0f20aca1df34e1911d18e
jczetta <jczetta@gmail.com>
2018-09-14 11:30:01 -0400

    Fix

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,6 @@
+import random
+
 def ask_question():
     question = input("What is your question? ")
-    return "It is decidedly so."
+    answers = ["It is certain.","It is decidedly so.","Without a doubt.","Yes-definitely.","You may rely on it.","As I see it, yes.", "Most likely.","Outlook good.","Yes.","Ask again later.","Reply hazy, try again","Concentrate and ask again","Don't count on it.","Very doubtful","My sources say no"]
+    return random.choice(answers)
